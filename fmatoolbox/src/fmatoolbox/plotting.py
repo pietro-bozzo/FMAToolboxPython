5a211d0477b0ca6db8162befba9be59272c4e896
pietro <pietro.bozzo1998@gmail.com>
2026-02-10 15:49:51 +0100

    make plotColorMap

--- fmatoolbox/src/fmatoolbox/plotting.py
+++ fmatoolbox/src/fmatoolbox/plotting.py
@@ -1,7 +1,9 @@
 ''' Plotting utilities for publication grade figures '''
 
 import matplotlib.axes as matpla
-import matplotlib.pyplot as matplp
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
 
 
 def adjustAxes(axs: matpla.Axes):
@@ -39,7 +41,7 @@ def makeFigure(title: str, n: list[int] = [1,1], size: list[int] = [20,10]):
     #     axs    collection of matplotlib axes
 
     cm = 1 / 2.54 # inches to centimeter conversion factor
-    fig, axs = matplp.subplots(n[0],n[1],figsize=[size[0]*cm,size[1]*cm],constrained_layout=True)
+    fig, axs = plt.subplots(n[0],n[1],figsize=[size[0]*cm,size[1]*cm],constrained_layout=True)
 
     # promote single axis to iterable
     if isinstance(axs,matpla._axes.Axes):
@@ -49,3 +51,34 @@ def makeFigure(title: str, n: list[int] = [1,1], size: list[int] = [20,10]):
     adjustAxes(axs)
 
     return fig, axs
+
+
+def plotColorMap(data: npt.NDArray[np.floating], vmin = None, vmax = None, x = None, y = None, aspect = 3/4, ax = None):
+    # plot a colormap
+    #
+    # arguments:
+    #     data      (:,:) float, data to plot
+    #     vmin      float = None, colormap lower limit
+    #     vmax      float = None, colormap upper limit
+    #     x         (:) float = None, x values corresponding to columns of data
+    #     y         (:) float = None, y values corresponding to columns of data
+    #     aspect    float = 3/4, image aspect ratio
+    #     ax        Axes = plt.gca(), axes to plot in
+
+    if x is None:
+        x = [0,data.shape[1]]
+        dx = 0.5
+    else:
+        dx = (x[-1] - x[0]) / (data.shape[1] - 1) / 2
+    if y is None:
+        y = [0,data.shape[0]]
+        dy = 0.5
+    else:
+        dy = (y[-1] - y[0]) / (data.shape[0] - 1) / 2
+    if ax is None:
+        ax = plt.gca()
+
+    ax.set_aspect(aspect)
+    im = ax.imshow(data,aspect='auto',vmin=vmin,vmax=vmax,origin='lower',extent=[x[0]-dx,x[-1]+dx,y[0]-dy,y[-1]+dy])
+
+    return im
